d3dccd688e0a177abb1f2b4519049310eb4148c5
surister <surister98@gmail.com>
2019-01-09 23:10:26 +0100

    Add player.py

---
+++ player.py
@@ -0,0 +1,15 @@
+from constants import NoteConversion
+
+class Player:
+    def __init__(self, solfege=False):
+        self._check = ...
+
+    def _internal_check(self):
+        pass
+
+    def play(self, times: int = None) -> None:
+        pass
+
+
+class Song:
+    pass
